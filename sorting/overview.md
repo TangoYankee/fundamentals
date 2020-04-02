17560c30564310eda4d404cc9b70779f354ed6a4
Timothy Miller <miller.tim108@gmail.com>
2020-04-01 17:27:40 -0700

    compare radix time to built in tim sort (#9)

--- sorting/overview.md
+++ sorting/overview.md
@@ -23,3 +23,46 @@
   - Limited to lexicographical order. 
   - Requires additional memory. 
   - It will run in the same time, even when the list is already sorted.
+
+### Radix Compared to Python Tim Sort
+```
+"""Source:
+https://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Radix_sort"""
+
+from time import time as current_time
+from random import randint
+
+
+def radix_sort(a, n, maxLen):
+    for x in range(maxLen):
+        bins = [[] for i in range(n)]
+        for y in a:
+            bins[int(y/n**x) % n].append(y)
+        a = []
+        for section in bins:
+            a.extend(section)
+    return a
+
+
+def compare_sorts(data, n, maxLen):
+    start_time = current_time()
+    radix_sort(data, n, maxLen)
+    print(f"Radix time: { current_time() - start_time}")
+    start_time = current_time()
+    sorted(data)
+    print(f"Tim Sort time: {current_time() - start_time}")
+
+
+def main():
+    compare_sorts([randint(0, 100) for _ in range(10)], 9, 3)
+    # Radix time: 2.9087066650390625e-05
+    # Tim Sort time: 3.5762786865234375e-06
+    compare_sorts([randint(0, 100) for _ in range(10**4)], 10**6-1, 3)
+    # Radix time: 1.0376839637756348
+    # Tim Sort time: 0.0019342899322509766
+
+
+if __name__ == "__main__":
+    main()
+
+```
--- sorting/radix.py
+++ sorting/radix.py
@@ -1,37 +1,38 @@
 """Source:
-https://stackoverflow.com/questions/33901534/radix-sort-program-in-python"""
+https://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Radix_sort"""
+
+from time import time as current_time
+from random import randint
 
 
-def RadixSort(A):
-    RADIX = 10
-    maxLength = False
-    tmp, placement = -1, 1
-
-    while not maxLength:
-        maxLength = True
-        buckets = [list() for _ in range(RADIX)]
-        for i in A:
-            tmp = int(i / placement)
-            buckets[tmp % RADIX].append(i)
-            if maxLength and tmp > 0:
-                maxLength = False
-
-        a = 0
-        for b in range(RADIX):
-            buck = buckets[b]
-            for i in buck:
-                A[a] = i
-                a += 1
-        # move to next digit
-        placement *= RADIX
-    return A
+def radix_sort(a, n, maxLen):
+    for x in range(maxLen):
+        bins = [[] for i in range(n)]
+        for y in a:
+            bins[int(y/n**x) % n].append(y)
+        a = []
+        for section in bins:
+            a.extend(section)
+    return a
 
 
-A = []
-n = input("Enter the numebr of elements you want to sort : ")
-n = int(n)
-print("Enter the numbers : \n")
-for i in range(0, n):
-    num = int(input())
-    A.append(num)
-print(RadixSort(A))
+def compare_sorts(data, n, maxLen):
+    start_time = current_time()
+    radix_sort(data, n, maxLen)
+    print(f"Radix time: { current_time() - start_time}")
+    start_time = current_time()
+    sorted(data)
+    print(f"Tim Sort time: {current_time() - start_time}")
+
+
+def main():
+    compare_sorts([randint(0, 100) for _ in range(10)], 9, 3)
+    # Radix time: 2.9087066650390625e-05
+    # Tim Sort time: 3.5762786865234375e-06
+    compare_sorts([randint(0, 100) for _ in range(10**4)], 10**6-1, 3)
+    # Radix time: 1.0376839637756348
+    # Tim Sort time: 0.0019342899322509766
+
+
+if __name__ == "__main__":
+    main()
