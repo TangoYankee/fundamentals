db21dc6d3f14f4a8ecd2e096c9a17e776f46675a
Timothy Miller <miller.tim108@gmail.com>
2020-04-21 15:16:46 -0700

    lint, add route (#13)

--- index.md
+++ index.md
@@ -6,6 +6,7 @@
 1. [Concurrency](concurrency)
 2. [Hashing](hashing)
 3. [Sorting](sorting)
+4. [Linked Lists](linked-lists)
 
 ## Source Code
 1. [Render Markdown in Flask with Style](https://github.com/solitudenote/gitkeeper/blob/master/app.py)
--- linked-lists/reverse.py
+++ linked-lists/reverse.py
@@ -1,39 +1,44 @@
 class Node:
-  def __init__(self, value):
-    self.value = value
-    self.next = None
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 
 class LinkedList:
-  def __init__(self):
-    self.head = None
-  
-  def unshift(self, value):
-    node = Node(value)
-    node.next = self.head
-    self.head = node
-    
+    def __init__(self):
+        self.head = None
+
+    def unshift(self, value):
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+
+
 def create_linked_list():
-  llist = LinkedList()
-  for i in range(0,17,4):
-    llist.unshift(i)
-  return llist
+    llist = LinkedList()
+    for i in range(0, 17, 4):
+        llist.unshift(i)
+    return llist
+
 
 def display(head):
-  print("begin")
-  while(head):
-    print(head.value)
-    head = head.next
-  print("end")
+    print("begin")
+    while(head):
+        print(head.value)
+        head = head.next
+    print("end")
+
 
 def reverse_linked_list(head):
-  current = head
-  prev = None # Eventually end with Null
-  while(current):
-    temp = current.next # Temporary node to prevent next node getting lost
-    current.next = prev # Reverse pointer for current node
-    prev = current # Current node falls one position
-    current = temp # Find the temporary node
-  return prev # Holds the last valid reference to a node. Current is now null
+    current = head
+    prev = None
+    while(current):
+        temp = current.next
+        current.next = prev
+        prev = current
+        current = temp
+    return prev
+
 
 llist = create_linked_list()
 display(llist.head)
